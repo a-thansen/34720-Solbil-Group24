84576870773e1ba2414415fb72b7e8d7e316ca10
a-thansen <102360418+a-thansen@users.noreply.github.com>
2023-06-20 10:52:57 +0200

    Update input.h

--- include/input.h
+++ include/input.h
@@ -1,10 +1,6 @@
 
 void setupJack();
 void inputDetect();
-void pushToBack(int* array, int length, int value) ;
-void print_array(int* array, int length);
 int findStandardDeviation(int *array, int count);
-bool readAux(void *);
-bool probNoInput(void *);
 void setMux(int mode);
 
